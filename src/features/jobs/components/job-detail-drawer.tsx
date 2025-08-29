03130f7d31c317f318f2a9116b8f2c3a8a045de5
2323367923 <2323367923@qq.com>
2025-08-29 17:57:07 +0800

    refactor: ui question

--- src/features/jobs/components/job-detail-drawer.tsx
+++ src/features/jobs/components/job-detail-drawer.tsx
@@ -19,7 +19,7 @@ export default function JobDetailDrawer({
 }: JobDetailDrawerProps) {
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
-      <SheetContent className='flex min-h-0 w-full flex-col overflow-y-auto px-4 sm:max-w-none md:w-[85vw] md:px-5 lg:w-[60vw] xl:w-[50vw]'>
+      <SheetContent className='flex min-h-0 w-full flex-col overflow-y-auto px-4 sm:max-w-none md:w-[85vw] md:px-5 lg:w-[60vw] xl:w-[50vw] [&>button]:hidden'>
         <SheetTitle className='sr-only'>职位详情</SheetTitle>
         {job && (
           <JobDetailContent
