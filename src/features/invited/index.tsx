aac46493b3190934dbd1a86ed312ade1e8ed995d
2323367923 <2323367923@qq.com>
2025-08-29 17:26:27 +0800

    fix: onboarding page navigate

--- src/features/invited/index.tsx
+++ src/features/invited/index.tsx
@@ -1,18 +1,13 @@
-import { useEffect } from 'react'
-import { useRouter } from '@tanstack/react-router'
-import { useAuthStore } from '@/stores/authStore'
 import { Main } from '@/components/layout/main'
 import InvitedForm from '@/features/invited/components/invited-form'
 
 export default function InvitedPage() {
-  const user = useAuthStore((s) => s.auth.user)
-  const router = useRouter()
 
-  useEffect(() => {
-    if (user?.is_onboard) {
-      router.history.back()
-    }
-  }, [router.history, user])
+  // useEffect(() => {
+  //   if (!user?.is_onboard) {
+  //     router.history.back()
+  //   }
+  // }, [user])
 
   return (
     <Main className='flex h-full items-center justify-center'>
