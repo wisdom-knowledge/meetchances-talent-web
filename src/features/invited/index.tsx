f078a712fe4ac126196fbc0d416cb0b32808e22c
wangxu <wangxu@col.com>
2025-11-27 11:35:40 +0800

    fix：样式修改

--- src/features/invited/index.tsx
+++ src/features/invited/index.tsx
@@ -10,7 +10,7 @@ export default function InvitedPage() {
   // }, [user])
 
   return (
-    <Main className='flex-1 overflow-y-auto'>
+    <Main className='flex h-full items-center justify-center py-8'>
       <InvitedForm />
     </Main>
   )
